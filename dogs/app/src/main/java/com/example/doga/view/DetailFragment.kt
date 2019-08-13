6b819f6359801a504ed68ec446d2d14c8b0b5988
RONI <papai983g@gmail.com>
2019-08-13 08:50:53 +0530

    changes

--- dogs/app/src/main/java/com/example/doga/view/DetailFragment.kt
+++ dogs/app/src/main/java/com/example/doga/view/DetailFragment.kt
@@ -9,18 +9,28 @@ import android.view.ViewGroup
 import androidx.navigation.Navigation
 
 import com.example.doga.R
-import kotlinx.android.synthetic.main.fragment_list.*
+import kotlinx.android.synthetic.main.fragment_detail.*
 
 
-class ListFragment : Fragment() {
+class DetailFragment : Fragment() {
+
+    private var dogUUID = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        return inflater.inflate(R.layout.fragment_detail, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        arguments?.let {
+            dogUUID = DetailFragmentArgs.fromBundle(it).dogUuid
+        }
+
+
+    }
 }
