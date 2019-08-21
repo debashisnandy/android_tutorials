bc4e9b061237b81f413e3ec88c5dd4b870143bd9
RONI <papai983g@gmail.com>
2019-08-21 09:57:08 +0530

    Preferences

--- dogs/app/src/main/java/com/example/doga/view/ListFragment.kt
+++ dogs/app/src/main/java/com/example/doga/view/ListFragment.kt
@@ -2,12 +2,11 @@ package com.example.doga.view
 
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.doga.R
 import com.example.doga.viewmodel.ListViewModel
@@ -23,6 +22,7 @@ class ListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        setHasOptionsMenu(true)
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
@@ -45,6 +45,8 @@ class ListFragment : Fragment() {
             viewModel.refreshBypassCache()
             refreshLayout.isRefreshing = false
         }
+
+
 
         observeViewModel()
     }
@@ -73,5 +75,22 @@ class ListFragment : Fragment() {
                 }
             }
         })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.item_menu,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.actionSettings ->{
+                view?.let {
+                    val action = ListFragmentDirections.actionSettingsFragment()
+                    Navigation.findNavController(it).navigate(action)
+                }
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
